e227569d0c1a5f08f4622271e9f93a5c3b51eea8
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-09-11 14:19:38 +0300

    Add publish integration

---
+++ publish.ps1
@@ -0,0 +1,3 @@
+$moduleName = 'SPS-Zabbix-Host'
+$moduleSrcPath = Join-Path -Path $PSScriptRoot -ChildPath $moduleName
+Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY
