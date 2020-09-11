51bcc56d1100da0667c38c60eaf62e9f40c19a20
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-09-11 14:21:46 +0300

    fix module name

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
-$moduleName = 'SPS-Zabbix-Host'
+$moduleName = 'PS-Zabbix-Host'
 $moduleSrcPath = Join-Path -Path $PSScriptRoot -ChildPath $moduleName
 Publish-Module -Path $moduleSrcPath -NuGetApiKey $env:POWERSHELL_GALLERY_API_KEY
